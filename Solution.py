83aef3aca0571f3c030b71a03a9837dd5bea022d
vaibhavsurfer8888 <75718233+vaibhavsurfer8888@users.noreply.github.com>
2021-06-22 12:57:14 -0400

    Update Solution.py
    added isPalindrome function which checks if number is a palindrome number

--- Solution.py
+++ Solution.py
@@ -18,7 +18,6 @@ class Solution(object):
         self.nums = nums
         self.target = target
         
-        
         for j in range(len(nums)):
             for i in range(len(nums)):
                 if i != j:
@@ -30,3 +29,28 @@ class Solution(object):
                         continue
                 else: 
                     continue
+                    
+                    
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+                    
+        self.x = x
+        
+        if x>=0:
+            # convert to string to access elements
+            x_str = str(x)
+            reverse_str = x_str[::-1]
+            reverse_x = int(reverse_str)
+            if x == reverse_x:
+                output = True
+                return output
+            else:
+                output = False
+                return output
+        else:
+            output = False
+            return output
+                
